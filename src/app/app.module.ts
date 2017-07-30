87ea76fe3741e59ce1c0f37bc03bd6dc036b6fdc
antpowell <powell.anthony.e@gmail.com>
2017-07-30 16:31:38 -0400

    Angular 4 First Sit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
