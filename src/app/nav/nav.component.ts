1cb39a18221459134646412b35dcd0b676c1f384
Powell, Anthony <powell.anthony.e@gmail.com>
2017-08-05 20:32:30 -0500

    add ngFor for headings in nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  homeValue = 'Home';
-  headings = ['ABOUT', 'CONTACT', 'YOU'];
+  homeValue = 'HOME';
+  headings = ['About', 'Contact', 'Resume'];
   constructor() { }
 
   ngOnInit() {
